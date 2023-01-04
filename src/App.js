f4118a046093e94684d43e8ddccf9b4f540da83e
Nikita <lavro.nikita123@gmail.com>
2023-01-04 17:05:49 -0500

    App initial routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+//CSS
 import "./App.css";
+
+//Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Material-UI
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:search" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
