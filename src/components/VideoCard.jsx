566cd029a11a77f1362f4306c3a0bc60ea4dde2d
Nikita <lavro.nikita123@gmail.com>
2023-01-18 22:22:46 -0500

    VideoCard component imports

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
-const VideoCard = () => {
+//MUI
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+//Router
+import { Link } from "react-router-dom";
+
+//Utils
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({ video }) => {
   return <div>VideoCard</div>;
 };
 
