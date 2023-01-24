dc63d2a70b8c7c49c48cae5cb64f44c087ff47cc
Nikita <lavro.nikita123@gmail.com>
2023-01-23 19:22:38 -0500

    ChannelDetails fetching videos

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -12,14 +12,19 @@ import { fetchFromApi } from "../utils/fetchFromApi";
 
 const ChannelDetail = () => {
   const [channelDetails, setChannelDetails] = useState(null);
+  const [videos, setVideos] = useState([]);
+
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromApi(`channels?part="snippet&id=${id}`).then((data) =>
       setChannelDetails(data?.items[0])
     );
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
   }, [id]);
-  console.log(channelDetails);
+  console.log(videos);
 
   return <div>{id}</div>;
 };
