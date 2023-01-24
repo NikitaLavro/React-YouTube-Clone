f3fd76cad65af51bd505ede389aa43f184580dac
Nikita <lavro.nikita123@gmail.com>
2023-01-23 19:09:27 -0500

    ChannelDetail component imports

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+//Routes
+import { useParams } from "react-router-dom";
+
+//MUI
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "/";
 
 const ChannelDetail = () => {
   return <div>ChannelDetail</div>;
