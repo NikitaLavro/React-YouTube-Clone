9239165df662084994081875ee7fd25c3c4b6c26
Nikita <lavro.nikita123@gmail.com>
2023-02-09 20:00:32 +0200

    SearchFeed component initial structure

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+//MUI
+import { Box, Typography } from "@mui/material";
+
+//Components
+import { Videos } from "/";
+
+//Utils
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=`).then((data) => {
+      setVideos(data.items);
+    });
+  }, []);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" md={2} sx={{ color: "white" }}>
+        Search Results for: <span style={{ color: "#F31503" }}>Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
